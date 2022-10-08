f932fbab5359e0f71735da0f48cbdb849dbcb439
jiaozi <1296724559@qq.com>
2022-10-08 17:41:20 +0800

    chore: 更新版本

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+export type { MasonryProps } from './masonry';
 export {
   default as Masonry,
   MasonryAbsoluteItem,
   MasonryItem
 } from './masonry';
-export type { MasonryProps } from './masonry';
+export type { MasonryScrollProps } from './masonry-scroll';
+export {
+  default as MasonryScrill
+} from './masonry-scroll';
